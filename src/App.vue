a98bc6c0255ec662c0ccd12fcb173f92e7663513
HolodTema <holod.tema13062006@gmail.com>
2023-01-26 22:41:42 +0800

    There are smth kinda blog. I saved it to remember how to create reactive pattern in code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <form>
+      <h4>Post creating</h4>
+      <input class="input"
+             type="text"
+             placeholder="Title"
+             v-bind:value="title"
+             @input="inputTitle">
+      <input class="input" type="text" placeholder="Description" v-bind:value="body">
+      <button class="btn" @click="createPost">Create</button>
+    </form>
+    <div class="post" v-for="post in posts" v-bind:key="post.id">
+      <div><strong>Title: </strong>{{post.title}}</div>
+      <div><strong>Description: </strong>{{post.body}}</div>
+    </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'description'},
+        {id: 2, title: 'JavaScript 2', body: 'description 2'},
+        {id: 3, title: 'JavaScript 3', body: 'description 3'},
+        {id: 3, title: 'JavaScript 4', body: 'description 4'},
+        {id: 3, title: 'JavaScript 5', body: 'description 5'},
+      ],
+      title: '',
+      body: '',
+    }
+  },
+
+  methods: {
+    createPost() {
+
+    },
+
+    inputTitle(event) {
+      this.title = event.target.value;
+    }
   }
 }
 </script>
-
+<!--scoped is like private - access for styles will be only for this components-->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+
 }
 </style>
